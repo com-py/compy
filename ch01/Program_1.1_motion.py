3bedcf9761a8c0340cd464b70580a6230ea4e99f
com-py <jw@LT-322532-PHY.UMDAR.umassd.edu>
2015-04-21 14:40:47 -0400

    progs
    added progs

---
+++ ch01/Program_1.1_motion.py
@@ -0,0 +1,21 @@
+#
+# Program 1.1: Motion with constant acceleration (motion.py)
+# J Wang, Computational modeling and visualization with Python
+#
+
+import matplotlib.pyplot as plt # get matplotlib plot functions 
+
+a, v0 = input('enter a, v0 (eg 1.0, 0.0) : ')   # read input 
+t, h, n = 0.0, 0.1, 20          # init time, step size, number of steps
+ta, xa = [], []                 # time and position arrays for plotting 
+
+for i in range(n):              # loop for n steps  
+    ta.append(t)                # record time and position
+    xa.append(v0*t + a*t*t/2.0)
+    t = t + h                   # update time 
+
+plt.figure()                    # start a figure; no indent->end of loop    
+plt.plot(ta, xa, '-o')          # plot data
+plt.xlabel('t (s)')             # add labels
+plt.ylabel('x (m)')
+plt.show()                      # show figur
---
+++ ch02/Program_2.1_bouncing_ball.py
@@ -0,0 +1,21 @@
+#
+# Program 2.1: A bouncing ball (bouncing_ball.py)
+# J Wang, Computational modeling and visualization with Python
+#
+
+import visual as vp     # get VPython modules for animation
+
+# draw the ball at (0,5,0) and a thin floor at (0,-5,0)
+ball = vp.sphere(pos=(0,5,0), radius=1, color=vp.color.yellow) # ball  
+floor = vp.box(pos=(0,-5,0), length=8, height=0.2, width=4)    # floor 
+
+h = 0.01            # time step size                        
+v = 0.0             # initial velocity                      
+while True:         # loop forever
+    vp.rate(400)    # limit animation rate to 400 loops/sec 
+    ball.pos.y = ball.pos.y + v*h       # update y position 
+    if ball.pos.y > floor.y + ball.radius:  
+        v = v - 9.8*h       # above floor, update velocity  
+    else:                               
+        v = - v             # below floor, reverse velocity 
+       
